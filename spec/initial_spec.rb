5af5ab9cfbb6fbcf37939bac914cc27af609b580
Stephan Kämper <the.tester@seasidetesting.com>
2021-02-28 19:25:38 +0100

    Uses RSpec context

--- spec/initial_spec.rb
+++ spec/initial_spec.rb
@@ -2,8 +2,10 @@
 
 require_relative 'spec_helper'
 
-describe 'ProductInfoHandler' do
-  it 'instantiates a product info handler' do
-    expect { ProductInfoHandler.new }.not_to raise_exception
+describe ProductInfoHandler do
+  context 'initialisation' do
+    it 'instantiates a product info handler' do
+      expect { ProductInfoHandler.new }.not_to raise_exception
+    end
   end
 end
