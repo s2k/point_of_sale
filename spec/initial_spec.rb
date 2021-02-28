7ae70bde2f7f76d5f73ad583d963e181139af7b9
Stephan Kämper <the.tester@seasidetesting.com>
2021-02-28 19:19:08 +0100

    Renames initial test

--- spec/initial_spec.rb
+++ spec/initial_spec.rb
@@ -3,7 +3,7 @@
 require_relative 'spec_helper'
 
 describe 'ProductInfoHandler' do
-  it 'Can instantiate a product info handler' do
+  it 'instantiates a product info handler' do
     expect { ProductInfoHandler.new }.not_to raise_exception
   end
 end
