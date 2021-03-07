ee5f98b130ffd01db7f1c01a89452c2903d09af8
Stephan Kämper <the.tester@seasidetesting.com>
2021-03-07 11:32:26 +0100

    Adds some docs

--- lib/product_info_handler.rb
+++ lib/product_info_handler.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+# A ProductInfoHandler accepts input from (presumably) a bar code reader and sends the
+# passed information to subscribed objects.
+# I assume at least one thing will subscribe to this: A service that uses the read product ID
+# queries another service for more details product information… which is finally displayed
+# on an LCD screen of sorts.
+#
 class ProductInfoHandler
   def on_barcode(read_barcode = nil)
     # code here
