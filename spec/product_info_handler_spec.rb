d26e9c5116f47e7285eeb7c3b1bd2b3e878ab8cd
Stephan Kämper <the.tester@seasidetesting.com>
2021-03-07 20:19:44 +0100

    Refactor repeatedly used test data into constants.

--- spec/product_info_handler_spec.rb
+++ spec/product_info_handler_spec.rb
@@ -2,9 +2,11 @@
 
 require_relative 'spec_helper'
 
-describe ProductInfoHandler do
-  MAX_ID = 2**(0.size * 8)
+MAX_ID = 2**(0.size * 8)
+MESSAGE_TEXT = "Product: Thingy\nPrice: 47.11"
+VALID_BARCODE = "47856\n"
 
+describe ProductInfoHandler do
   before(:example) do
     @product_info_service = instance_double('ProductInformationService')
     allow(@product_info_service).to receive :find_product_info_for
@@ -27,8 +29,8 @@ describe ProductInfoHandler do
 
   context 'getting product information (price in particular) from … elsewhere' do
     it 'Again: It can be instantiated' do
-      expect(@product_info_service).to receive(:find_product_info_for).with('47856').and_return( "Product: Thingy\nPrice: 47.11" )
-      expect(@product_info_handler.on_barcode("47856\n")).to be_nil
+      expect(@product_info_service).to receive(:find_product_info_for).with('47856').and_return(MESSAGE_TEXT)
+      expect(@product_info_handler.on_barcode(VALID_BARCODE)).to be_nil
     end
   end
 
@@ -39,33 +41,31 @@ describe ProductInfoHandler do
     end
 
     it 'Accepts subscriber' do
-      expect{@product_info_handler.subscribe @subscriber}.to_not raise_exception
+      expect { @product_info_handler.subscribe @subscriber }.to_not raise_exception
     end
 
     it 'Notifies subscriber when a barcode event happens' do
-      expect(@product_info_service).to receive(:find_product_info_for).with('47856').and_return("Product: Thingy\nPrice: 47.11")
-      expect{@product_info_handler.subscribe @subscriber}.to_not raise_exception
-      expect(@subscriber).to receive(:update).with("Product: Thingy\nPrice: 47.11")
-      @product_info_handler.on_barcode("47856\n")
+      expect(@product_info_service).to receive(:find_product_info_for).with('47856').and_return(MESSAGE_TEXT)
+      expect { @product_info_handler.subscribe @subscriber }.to_not raise_exception
+      expect(@subscriber).to receive(:update).with(MESSAGE_TEXT)
+      @product_info_handler.on_barcode(VALID_BARCODE)
     end
 
     it 'No one is notified, when nobody subscribes' do
-      expect(@product_info_service).to receive(:find_product_info_for).with('47856').and_return("Product: Thingy\nPrice: 47.11")
+      expect(@product_info_service).to receive(:find_product_info_for).with('47856').and_return(MESSAGE_TEXT)
       expect(@subscriber).not_to receive(:update)
-      @product_info_handler.on_barcode("47856\n")
+      @product_info_handler.on_barcode(VALID_BARCODE)
     end
-
 
     it 'Notifies all subscribers when a barcode event happens' do
       @subscriber2 = instance_double('Display')
       subscribers = [@subscriber, @subscriber2]
-      expect(@product_info_service).to receive(:find_product_info_for).once.with('47856').and_return("Product: Thingy\nPrice: 47.11")
+      expect(@product_info_service).to receive(:find_product_info_for).once.with('47856').and_return(MESSAGE_TEXT)
       subscribers.each do |s|
         expect { @product_info_handler.subscribe s }.to_not raise_exception
-        expect(s).to receive(:update).with("Product: Thingy\nPrice: 47.11")
+        expect(s).to receive(:update).with(MESSAGE_TEXT)
       end
-      @product_info_handler.on_barcode("47856\n")
+      @product_info_handler.on_barcode(VALID_BARCODE)
     end
-
   end
 end
